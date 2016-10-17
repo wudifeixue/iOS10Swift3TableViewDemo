be92ced6ca0459e84cffd9988ed62db7f93a3afa
Avan-tech Mac <pengzho@gmail.com>
2016-10-17 15:41:56 -0600

    Add encoding and decoding method

--- iOS10Swift3TableViewDemo/Object.swift
+++ iOS10Swift3TableViewDemo/Object.swift
@@ -10,13 +10,54 @@ import Foundation
 
 class Object : NSObject, NSCoding {
     
-    //override init() {}
-    
-    required init(coder decoder: NSCoder) {
-        
+    struct Keys {
+        static let Name = "name";
+        static let LastName = "lastName";
     }
     
-    func encode(with coder: NSCoder) {
+    private var _name = "";
+    private var _lastName = "";
+    
+    override init() {}
+    
+    init(name: String, lastName: String) {
+        self._name = name;
+        self._lastName = lastName;
+    }
+    
+    //loading
+    required init(coder decoder: NSCoder) {
+        if let nameObj = decoder.decodeObject(forKey: Keys.Name) as? String {
+            _name = nameObj;
+        }
         
+        if let lastNameObj = decoder.decodeObject(forKey: Keys.LastName) as? String {
+            _lastName = lastNameObj;
+        }
+    }
+    
+    //saving
+    func encode(with coder: NSCoder) {
+        coder.encode(_name, forKey: Keys.Name);
+        coder.encode(_lastName, forKey: Keys.LastName);
+    }
+    
+    var Name: String {
+        get {
+            return _name;
+        }
+        
+        set{
+            _name = newValue;
+        }
+    }
+    
+    var lastName: String {
+        get {
+            return _lastName;
+        }
+        set {
+            _lastName = newValue;
+        }
     }
 }
