c0df6e5a04ec89084a9fb9841e346065a13d076b
Avan-tech Mac <pengzho@gmail.com>
2016-10-17 15:23:32 -0600

    Add an object class

---
+++ iOS10Swift3TableViewDemo/Object.swift
@@ -0,0 +1,22 @@
+//
+//  Object.swift
+//  iOS10Swift3TableViewDemo
+//
+//  Created by Avantech on 2016-10-17.
+//  Copyright © 2016 wudifeixue. All rights reserved.
+//
+
+import Foundation
+
+class Object : NSObject, NSCoding {
+    
+    //override init() {}
+    
+    required init(coder decoder: NSCoder) {
+        
+    }
+    
+    func encode(with coder: NSCoder) {
+        
+    }
+}
